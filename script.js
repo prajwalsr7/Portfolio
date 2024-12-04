8853e8649eb4703d55a3176ca19a20e7ed48df06
Prajwal Sr <prajwalsr7@gmail.com>
2024-12-04 23:24:12 +0000

    3rd commit

--- script.js
+++ script.js
@@ -3,14 +3,22 @@ async function loadProjects() {
         const response = await fetch('http://localhost:5000/projects');
         const projects = await response.json();
 
-        const container = document.querySelector('.project-container');
+        const container = document.querySelector('.container2');
         projects.forEach(project => {
             const projectCard = `
-                <div class="project-card">
-                    <h2>${project.name}</h2>
-                    <p>${project.description}</p>
-                    <a href="${project.report_pdf}" target="_blank">View Report</a>
-                    <a href="${project.github_link}" target="_blank">GitHub Repo</a>
+                <div class="card">
+                    <div class="face face1">
+                        <div class="content">
+                            <h3>${project.name}</h3>
+                        </div>
+                    </div>
+                    <div class="face face2">
+                        <div class="content">
+                            <p>${project.description}</p>
+                                <a href="${project.report_pdf}">View Report</a>
+                                <a href="${project.github_link}">GitHub Repo</a>
+                        </div>
+                    </div>
                 </div>
             `;
             container.innerHTML += projectCard;
